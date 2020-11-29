9e83cf9ecc72b17442c4a406217c8a78baafa019
洪文辉 <Eden445129997@163.com>
2020-11-29 23:38:32 +0800

    test commit

--- main/settings/beta/language.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# 国际化/语言
-# from django.conf.global_settings import LANGUAGES
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh-hans'
-# 启用 or 不启用
-USE_I18N = True
